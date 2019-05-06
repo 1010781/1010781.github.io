d96dce8958560a08b80c40b18f72048424dd67f1
1010781 <1010781@schoolsnet.act.edu.au>
2019-05-06 15:28:53 +1000

    oh yeah yeah yeah yeah

--- Environment.js
+++ Environment.js
@@ -21,7 +21,48 @@ function changeButton() {
     var txt = myButton.textContent || myButton.innerText;
     if (txt.match("Enable")) {
         myButton.textContent = "Disable";
+        myButton.style.backgroundColor = "red";
     } else {
         myButton.textContent = "Enable";
+        myButton.style.backgroundColor = "green";
+
+    }
+}
+
+function powerStation1() {
+    var currentStatus = [Math.floor(Math.random()*1)]
+    var currentPowerload = [Math.floor(Math.random()*3)]
+    var currentPercentage = [Math.floor(Math.random()*3)]
+    var reccomendationUsage = [Math.floor(Math.random()*3)]
+
+    if (currentStatus <=1) {
+        document.getElementById('CurrentStatus').innerHTML = 'Enable';
+        document.getElementById('CurrentStatus').style.color = 'green';
+    }
+
+    if (currentPowerload <=1) {
+        document.getElementById('CurrentPowerload').innerHTML = 'Low';
+        document.getElementById('CurrentPowerload').style.color = 'green';
+    } else if (currentPowerload <=2) {
+        document.getElementById('CurrentPowerload').innerHTML = 'Medium';
+        document.getElementById('CurrentPowerload').style.color = 'orange';
+    } else if (currentPowerload <=3) {
+        document.getElementById('CurrentPowerload').innerHTML = 'High';
+        document.getElementById('CurrentPowerload').style.color = 'red';
+    }
+
+    if (currentPercentage <=1) {
+        document.getElementById('CurrentPercentage').innerHTML = 'Poorly';
+    }
+
+    if (reccomendationUsage <=1) {
+        document.getElementById('ReccomendationUsage').innerHTML = 'Low';
+        document.getElementById('ReccomendationUsage').style.color = 'red';
+    } else if (reccomendationUsage <=2) {
+        document.getElementById('ReccomendationUsage').innerHTML = 'Medium';
+        document.getElementById('ReccomendationUsage').style.color = 'red';
+    } else if (reccomendationUsage <=3) {
+        document.getElementById('ReccomendationUsage').innerHTML = 'High';
+        document.getElementById('ReccomendationUsage').style.color = 'red';
     }
 }
