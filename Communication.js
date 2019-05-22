49771d8ca3e5f1b0390bc0adbdfa87a29b2959d6
1010781 <1010781@schoolsnet.act.edu.au>
2019-05-22 12:33:15 +1000

    oh yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah

--- Communication.js
+++ Communication.js
@@ -1,127 +1,74 @@
-function checkPin() {
-    var username, password, button, invalid;
-    username = document.getElementById("userName").value;
-    if (username == "admin") {
-        button = "<p><button type=\"button\" class=\"btn btn-success\" style=\"margin-top: 80px; color: black\" id='MyButton' onclick=\"changeButton()\">Enable</button></p>";
-    } else {
-        invalid = "Incorrect Username or Password";
-    }
-    password = document.getElementById("passWord").value;
-    if (password == "Password1234") {
-        button = "<p><button type=\"button\"  class=\"btn btn-success\" style=\"margin-top: 80px; color: black;\" id='MyButton' onclick=\"changeButton()\">Enable</button></p>";
-    } else {
-        invalid = "Incorrect Username or Password";
-    }
-    document.getElementById("button").innerHTML = button;
-    document.getElementById("invalid").innerHTML = invalid;
-}
+function heatMap() {
+    var nbn = [Math.floor(Math.random() * 3)];
+    var adsl = [Math.floor(Math.random() * 3)];
+    var television = [Math.floor(Math.random() * 3)];
+    var phone = [Math.floor(Math.random() * 3)];
+    var radio = [Math.floor(Math.random() * 3)];
+    var foxtel = [Math.floor(Math.random() * 3)];
 
-function changeButton() {
-    var myButton = document.getElementById('MyButton');
-    var txt = myButton.textContent || myButton.innerText;
-
-    if (txt.match("Enable")) {
-        myButton.textContent = "Disable";
-        myButton.style.backgroundColor = "red";
-        powerStation2()
-    } else {
-        myButton.textContent = "Enable";
-        myButton.style.backgroundColor = "green";
-        powerStation2disable()
-    }
-}
-
-function powerStation1() {
-    var currentStatus = [Math.floor(Math.random()*1)];
-    var currentPowerload = [Math.floor(Math.random()*6)];
-    var currentPercentage = document.getElementById("CurrentPercentage")
-    var reccomendationUsage = [Math.floor(Math.random()*3)];
-
-    if (currentStatus <=1) {
-        document.getElementById('CurrentStatus').innerHTML = 'Enable';
-        document.getElementById('CurrentStatus').style.color = 'green';
+    if (nbn <=1) {
+        document.getElementById('Nbn').innerHTML = 'Great';
+        document.getElementById('Nbn').style.color = 'green';
+    } else if (nbn <=2) {
+        document.getElementById('Nbn').innerHTML = 'Okay';
+        document.getElementById('Nbn').style.color = 'orange';
+    } else if (nbn <=3) {
+        document.getElementById('Nbn').innerHTML = 'Poorly';
+        document.getElementById('Nbn').style.color = 'red';
     }
 
-    if (currentPowerload <=2) {
-        document.getElementById('CurrentPowerload').innerHTML = 'Low';
-        document.getElementById('CurrentPowerload').style.color = 'green';
-    } else if (currentPowerload <=4) {
-        document.getElementById('CurrentPowerload').innerHTML = 'Medium';
-        document.getElementById('CurrentPowerload').style.color = 'orange';
-    } else if (currentPowerload <=6) {
-        document.getElementById('CurrentPowerload').innerHTML = 'High';
-        document.getElementById('CurrentPowerload').style.color = 'red';
+    if (adsl <=1) {
+        document.getElementById('Adsl').innerHTML = 'Great';
+        document.getElementById('Adsl').style.color = 'green';
+    } else if (adsl <=2) {
+        document.getElementById('Adsl').innerHTML = 'Okay';
+        document.getElementById('Adsl').style.color = 'orange';
+    } else if (adsl <=3) {
+        document.getElementById('Adsl').innerHTML = 'Poorly';
+        document.getElementById('Adsl').style.color = 'red';
     }
 
-    if (reccomendationUsage <=1) {
-        document.getElementById('ReccomendationUsage').innerHTML = 'Low';
-    } else if (reccomendationUsage <=2) {
-        document.getElementById('ReccomendationUsage').innerHTML = 'Medium';
-    } else if (reccomendationUsage <=3) {
-        document.getElementById('ReccomendationUsage').innerHTML = 'High';
-    }
-    currentPercentage.innerHTML = (Math.floor(Math.random()*100)+1);
-}
-
-function powerStation2() {
-    var currentStatus = [Math.floor(Math.random() * 1)];
-    var currentPowerload = [Math.floor(Math.random() * 6)];
-    var currentPercentage = document.getElementById("CurrentPercentage2")
-
-
-    if (currentStatus <= 1) {
-        document.getElementById('CurrentStatus2').innerHTML = 'Enable';
-        document.getElementById('CurrentStatus2').style.color = 'green';
+    if (television <=1) {
+        document.getElementById('Television').innerHTML = 'Great';
+        document.getElementById('Television').style.color = 'green';
+    } else if (television <=2) {
+        document.getElementById('Television').innerHTML = 'Okay';
+        document.getElementById('Television').style.color = 'orange';
+    } else if (television <=3) {
+        document.getElementById('Television').innerHTML = 'Poorly';
+        document.getElementById('Television').style.color = 'red';
     }
 
-    if (currentPowerload <= 2) {
-        document.getElementById('CurrentPowerload2').innerHTML = 'Low';
-        document.getElementById('CurrentPowerload2').style.color = 'green';
-    } else if (currentPowerload <= 4) {
-        document.getElementById('CurrentPowerload2').innerHTML = 'Medium';
-        document.getElementById('CurrentPowerload2').style.color = 'orange';
-    } else if (currentPowerload <= 6) {
-        document.getElementById('CurrentPowerload2').innerHTML = 'High';
-        document.getElementById('CurrentPowerload2').style.color = 'red';
+    if (phone <=1) {
+        document.getElementById('Phone').innerHTML = 'Great';
+        document.getElementById('Phone').style.color = 'green';
+    } else if (phone <=2) {
+        document.getElementById('Phone').innerHTML = 'Okay';
+        document.getElementById('Phone').style.color = 'orange';
+    } else if (phone <=3) {
+        document.getElementById('Phone').innerHTML = 'Poorly';
+        document.getElementById('Phone').style.color = 'red';
     }
 
-    currentPercentage.innerHTML = (Math.floor(Math.random() * 100) + 1);
-}
-
-function powerStation2disable() {
-    var currentStatus = [Math.floor(Math.random() * 1)];
-    var currentPowerload = [Math.floor(Math.random() * 1)];
-    var currentPercentage = [Math.floor(Math.random() * 1)];
-
-
-    if (currentStatus <= 1) {
-        document.getElementById('CurrentStatus2').innerHTML = 'Disable';
+    if (radio <=1) {
+        document.getElementById('Radio').innerHTML = 'Great';
+        document.getElementById('Radio').style.color = 'green';
+    } else if (radio <=2) {
+        document.getElementById('Radio').innerHTML = 'Okay';
+        document.getElementById('Radio').style.color = 'orange';
+    } else if (radio <=3) {
+        document.getElementById('Radio').innerHTML = 'Poorly';
+        document.getElementById('Radio').style.color = 'red';
     }
 
-    if (currentPowerload <= 1) {
-        document.getElementById('CurrentPowerload2').innerHTML = 'N/A';
-    }
-
-    if (currentPercentage <= 1) {
-        document.getElementById('CurrentPercentage2').innerHTML = 'N/A';
-    }
-}
-
-function powerStation2display() {
-    var currentStatus = [Math.floor(Math.random() * 1)];
-    var currentPowerload = [Math.floor(Math.random() * 1)];
-    var currentPercentage = [Math.floor(Math.random() * 1)];
-
-
-    if (currentStatus <= 1) {
-        document.getElementById('CurrentStatus2').innerHTML = 'Disable';
-    }
-
-    if (currentPowerload <= 1) {
-        document.getElementById('CurrentPowerload2').innerHTML = 'N/A';
-    }
-
-    if (currentPercentage <= 1) {
-        document.getElementById('CurrentPercentage2').innerHTML = 'N/A';
+    if (foxtel <=1) {
+        document.getElementById('Foxtel').innerHTML = 'Great';
+        document.getElementById('Foxtel').style.color = 'green';
+    } else if (foxtel <=2) {
+        document.getElementById('Foxtel').innerHTML = 'Okay';
+        document.getElementById('Foxtel').style.color = 'orange';
+    } else if (foxtel <=3) {
+        document.getElementById('Foxtel').innerHTML = 'Poorly';
+        document.getElementById('Foxtel').style.color = 'red';
     }
 }
