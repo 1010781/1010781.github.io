2ca67a5fba31707a0d0da7d1caa9b8dbf3f17cd7
1010781 <1010781@schoolsnet.act.edu.au>
2019-05-29 12:30:58 +1000

    oh yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah

--- Environment.js
+++ Environment.js
@@ -1,11 +1,5 @@
 function checkPin() {
-    var username, password, button, invalid;
-    username = document.getElementById("userName").value;
-    if (username == "admin") {
-        button = "<p><button type=\"button\" class=\"btn btn-success\" style=\"margin-top: 80px; color: black\" id='MyButton' onclick=\"changeButton()\">Enable</button></p>";
-    } else {
-        invalid = "Incorrect Username or Password";
-    }
+    var password, button, invalid;
     password = document.getElementById("passWord").value;
     if (password == "Password1234") {
         button = "<p><button type=\"button\"  class=\"btn btn-success\" style=\"margin-top: 80px; color: black;\" id='MyButton' onclick=\"changeButton()\">Enable</button></p>";
