0e9226d34b8a7257f51056eaa8983614a9f93711
1010781 <1010781@schoolsnet.act.edu.au>
2019-06-03 15:24:19 +1000

    oh yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah

--- Human.js
+++ Human.js
@@ -1,5 +1,5 @@
 function idRandomizer(){
-    var randomizer = [Math.floor(Math.random() * 6)];
+    var randomizer = [Math.floor(Math.random() * 7)];
 
     if (randomizer <=1) {
         document.getElementById('FirstName').value = 'Ryan';
@@ -23,67 +23,72 @@ function idRandomizer(){
         document.getElementById('MobileNumber').value = '0466 356 770';
         document.getElementById('HPhoneNumber').value = '6292566';
         document.getElementById('Address').value = '149/1 Anthony Rolfe Avenue, Gungahlin ACT 2912';
-        document.getElementById('Username').value = 'RyanNg145';
+        document.getElementById('Username').value = 'CatherRy909';
         document.getElementById('Password').value = '0cPweqvU';
         document.getElementById('Medicare').value = '6097 118571 1';
-        document.getElementById('Healthcare').value = '145 789 634A';
+        document.getElementById('Healthcare').value = '365 990 113A';
         document.getElementById('GPName').value = 'Dr Leokadia Sifiso';
 
     } else if (randomizer <=3) {
         document.getElementById('FirstName').value = 'Omar';
         document.getElementById('LastName').value = 'Kennedy';
-        document.getElementById('DateOfBirth').value = '2001-06-31';
+        document.getElementById('DateOfBirth').value = '2001-06-30';
         document.getElementById('Email').value = 'omarkennedy189@gmail.com';
         document.getElementById('MobileNumber').value = '0466 420 969';
         document.getElementById('HPhoneNumber').value = '62933491';
         document.getElementById('Address').value = '33 Henry Kendall Street, Franklin ACT 2913';
-        document.getElementById('Username').value = 'RyanNg145';
+        document.getElementById('Username').value = 'Okennedy12';
         document.getElementById('Password').value = 'D2f4XrwY';
         document.getElementById('Medicare').value = '5250 070161 1';
-        document.getElementById('Healthcare').value = '145 789 634A';
+        document.getElementById('Healthcare').value = '332 659 800';
         document.getElementById('GPName').value = 'Dr Gerd Ernestine';
 
     } else if (randomizer <=4) {
         document.getElementById('FirstName').value = 'Rueben';
         document.getElementById('LastName').value = 'Casey';
-        document.getElementById('DateOfBirth').value = '2001-09-29';
-        document.getElementById('Email').value = 'ryannguyen@gmail.com';
+        document.getElementById('DateOfBirth').value = '1977-02-23';
+        document.getElementById('Email').value = 'ruebencasey21@gmail.com';
         document.getElementById('MobileNumber').value = '0435 047 770';
         document.getElementById('HPhoneNumber').value = '62947854';
         document.getElementById('Address').value = '21/1 Mouat Street, Lyneham ACT 2602';
-        document.getElementById('Username').value = 'RyanNg145';
+        document.getElementById('Username').value = 'Rcasey101';
         document.getElementById('Password').value = 'gKN3nc6V';
         document.getElementById('Medicare').value = '3506 190281 1';
-        document.getElementById('Healthcare').value = '145 789 634A';
+        document.getElementById('Healthcare').value = '305 119 624';
         document.getElementById('GPName').value = 'Dr Shanene Dikla';
 
     } else if (randomizer <=5) {
         document.getElementById('FirstName').value = 'Mabel';
         document.getElementById('LastName').value = 'Thornton';
-        document.getElementById('DateOfBirth').value = '2001-09-29';
-        document.getElementById('Email').value = 'ryannguyen@gmail.com';
+        document.getElementById('DateOfBirth').value = '1994-08-14';
+        document.getElementById('Email').value = 'mabelthortnton08@gmail.com';
         document.getElementById('MobileNumber').value = '0443 022 564';
         document.getElementById('HPhoneNumber').value = '62928774';
         document.getElementById('Address').value = '1/66 Allara Street, City ACT 2601';
-        document.getElementById('Username').value = 'RyanNg145';
+        document.getElementById('Username').value = 'MabelT61';
         document.getElementById('Password').value = 'gVLzJa7v';
         document.getElementById('Medicare').value = '4618 655251 1';
-        document.getElementById('Healthcare').value = '145 789 634A';
+        document.getElementById('Healthcare').value = '443 999 768A';
         document.getElementById('GPName').value = 'Dr Zelda Żaneta';
 
     } else if (randomizer <=6) {
         document.getElementById('FirstName').value = 'Jenifer ';
         document.getElementById('LastName').value = 'Gibbs';
-        document.getElementById('DateOfBirth').value = '2001-09-29';
-        document.getElementById('Email').value = 'ryannguyen@gmail.com';
+        document.getElementById('DateOfBirth').value = '1988-07-30';
+        document.getElementById('Email').value = 'jenifergibbs142@gmail.com';
         document.getElementById('MobileNumber').value = '0466 237 733';
         document.getElementById('HPhoneNumber').value = '62949356';
         document.getElementById('Address').value = '10 Carrington Street, Deakin ACT 2600';
-        document.getElementById('Username').value = 'RyanNg145';
+        document.getElementById('Username').value = 'Jgibbs2199';
         document.getElementById('Password').value = 'kBtUj11Y';
         document.getElementById('Medicare').value = '5488 201681 1';
-        document.getElementById('Healthcare').value = '145 789 634A';
+        document.getElementById('Healthcare').value = '989 366 145A';
         document.getElementById('GPName').value = 'Dr Raoul Abraham';
     }
-    setTimeout('location.reload()',1000);
+}
+
+function submitButton() {
+    var text
+    text = hi;
+    document.getElementById("demo").innerHTML = text;
 }
